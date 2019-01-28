5f4f885d7df811db12fcafd13102572ef70e56e1
“MARIA <MDFORTINO@GMAIL.COM>
2019-01-28 14:36:24 -0500

    add test views for awareness posts

---
+++ views/app/index.js
@@ -0,0 +1,18 @@
+{{#if questions}}
+  <ul class="questions">
+    {{#each questions}}
+      <div class="card">
+        <div class="card-content">
+          <a class="card-title" href="/question/{{id}}">
+            {{ content }}
+          </a>
+        </div>
+        <div class="card-action">
+          <a href="/user/{{ author.id }}" class="">{{ author.email }}</a>
+          <span class="question__date">{{ createdAt }}</span>
+          <span class="question_answers">{{ answers.length }} Answers</span>
+        </div>
+      </div>
+    {{/each}}
+  </ul>
+{{/if}}
